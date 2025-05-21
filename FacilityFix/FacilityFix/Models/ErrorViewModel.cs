29a05870df6e27be043bcad74cbda9480e8e9c0f
Rafael Lafuente <164599781+rawrf01@users.noreply.github.com>
2025-05-22 00:06:42 +0800

    Setup

---
+++ FacilityFix/FacilityFix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FacilityFix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
